e0cefcb784bf44a96c6e350e4e34c1e5518bf78c
Matt Rostad <matt@rostad.ca>
2023-06-01 20:17:52 -0700

    delete .env

--- public/scripts/env.js
+++
@@ -1,2 +0,0 @@
-const bearerKey = "Bearer 3FxorwFkiispOQxQL72PTEX5wxO2oKMf6YuMHEPhNKpuaiUPIwF6-TchJlW3Q_9kLzM12ZJ1vn4XuY4Ib890nhB_Losa73PLo49J3g4qTmkj67o-Z7XqI6gRDXx2ZHYx"
-const rapidAPI = '976181ede8msh8db849df77064aep11246cjsn64c186ae1e1a'
